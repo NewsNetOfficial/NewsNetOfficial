13a24d79798cc1a9a02bb6f991f01ea8c9330de0
NewsNet <85984149+NewsNetOfficial@users.noreply.github.com>
2022-11-08 00:55:11 +0100

    Main

---
+++ script.js
@@ -0,0 +1,11 @@
+function wip() {
+  alert("Deze pagina is nog niet beschikbaar.");
+}
+
+function openInNewTab(url) {
+  window.open(url, '_blank').focus();
+}
+
+function nw() {
+  window.open("Belangrijke Brief.pdf", "_blank", "width=100%", "height=100%");
+}
